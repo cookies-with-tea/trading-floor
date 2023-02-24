de5bf879bd61795d8df6355f5a96b355ddd85b71
FrozZzy1 <frozzzy71@gmail.com>
2023-02-24 14:25:23 +0700

    add(src/apps/advertisement): add models: Advertisement, Type, Image

--- src/apps/advertisement/models.py
+++ src/apps/advertisement/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    title = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тип объявления'
+        verbose_name_plural = 'Типы объявлений'
+
+
+class Image(models.Model):
+    image = models.ImageField(...)
+    advertisement = models.ForeignKey('Advertisement')
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
+
+class Advertisement(models.Model):
+    title = models.TextField()
+    description = models.TextField(blank=True)
+    type = models.ManyToManyField(Type)
+    image = models.ForeignKey('Image')
+    urgency = models.Choices(...)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
