0f3bf029c134a1fbb5c75fed80f1dad24ce9d449
FrozZzy1 <frozzzy71@gmail.com>
2023-02-27 17:34:41 +0700

    update(src/apps/advertisement): update fields in the Advertisement

--- src/apps/advertisement/models.py
+++ src/apps/advertisement/models.py
@@ -17,8 +17,8 @@ class Image(models.Model):
 class Advertisement(models.Model):
     URGENCY_LIST = (
         ('URGENT', 'Срочно'),
-        ('NOT_SO_URGENT', 'Не очень срочно'),
-        ('NOT_AT_ALL_URGENT', 'Совсем не срочно'),
+        ('NSU', 'Не очень срочно'),
+        ('NAAU', 'Совсем не срочно'),
     )
 
     TYPE_LIST = (
@@ -31,8 +31,13 @@ class Advertisement(models.Model):
 
     title = models.TextField()
     description = models.TextField(blank=True)
-    type = MultiSelectField(choices=TYPE_LIST, max_choices=3)
-    image = models.ForeignKey('Image', blank=True)
+    type = MultiSelectField(choices=TYPE_LIST, max_choices=3, max_length=8)
+    image = models.ForeignKey(
+        'Image',
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='images',
+    )
     urgency = models.CharField(max_length=6, choices=URGENCY_LIST)
 
     def __str__(self):
