6e4ebd6c82bc3b182bf8e5a7b900b1bd6a110bdc
FrozZzy1 <frozzzy71@gmail.com>
2023-03-01 17:38:16 +0700

    delete(src/apps/user): delete permissions.py

--- src/apps/user/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
-
-
-class IsOwnerOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.user == request.user
