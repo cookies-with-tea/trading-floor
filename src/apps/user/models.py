106886e7fc5ad892ba57a4bb452bb5c6a77f5c35
FrozZzy1 <frozzzy71@gmail.com>
2023-03-01 12:34:53 +0700

    update(src/apps/user): update fields

--- src/apps/user/models.py
+++ src/apps/user/models.py
@@ -56,7 +56,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField('Почта', unique=True)
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField('Фамилия', max_length=20)
-    room_number = models.PositiveIntegerField('Номер комнаты', max_length=3)
+    room_number = models.PositiveIntegerField('Номер комнаты')
     avatar = ProcessedImageField(
         format='PNG',
         processors=[
