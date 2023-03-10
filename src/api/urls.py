fca800e2836aea5c77dfcb4e0a348c642437132f
FrozZzy1 <frozzzy71@gmail.com>
2023-03-10 15:38:29 +0700

    fix(src/api): delete slash

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('/users', include('api.v1.user.urls')),
-    path('/auth', include('api.common.auth.urls')),
+    path('users/', include('api.v1.user.urls')),
+    path('auth/', include('api.common.auth.urls')),
 ]
