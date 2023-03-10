dc17605540882a39db0f8493e069856839fb0a7b
FrozZzy1 <frozzzy71@gmail.com>
2023-03-10 15:41:21 +0700

    refactor(src/utils): refactor method email_normalization to is_correct_email_domain

--- src/utils/strings.py
+++ src/utils/strings.py
@@ -1,17 +1,2 @@
-import logging
-
-logger = logging.getLogger(__name__)
-
-
-def email_normalization(email):
-    email = email or ''
-    try:
-        email_name, domain_part = email.strip().rsplit('@', 1)
-    except ValueError:
-        logger.error('Incorrect email')
-    else:
-        if domain_part.lower() != 'mer.ci.nsu.ru':
-            raise TypeError('Email must end with a "@mer.ci.nsu.ru"')
-        email = f'{email_name}@{domain_part.lower()}'
-
-    return email
+def is_correct_email_domain(email: str, email_domain: str) -> bool:
+    return email.endswith(email_domain)
