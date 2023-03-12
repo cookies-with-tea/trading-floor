520bfbacace408a9ac852805665991654ff81a1c
s1veme <kpdpav@gmail.com>
2023-03-12 18:45:36 +0300

    update(src/api/v1/user/tests/test_view): add descriptions for test errors

--- src/api/v1/user/tests/test_view/test_register_view.py
+++ src/api/v1/user/tests/test_view/test_register_view.py
@@ -20,13 +20,15 @@ def test_user_registration(user_data: dict):
     response = api.post(url, user_data, format='json')
     response_content = response.json()
 
-    assert response.status_code == status.HTTP_201_CREATED
-    assert User.objects.count() == 1
+    assert response.status_code == status.HTTP_201_CREATED, 'Ожидался 201 статус-код ответа'
+    assert User.objects.count() == 1, 'Ожидалось, что количество пользователей в базе данных будет равно 1'
 
     user = User.objects.get(id=response_content['id'])
-    assert user.first_name == user_data['first_name']
-    assert user.password is not None
-    assert user.is_active is True
+    assert (
+        user.first_name == user_data['first_name']
+    ), 'Имя из тела ответа не равен имени пользователя, которое пришло в ответе'
+    assert user.password is not None, 'Ожидалось, что в теле ответа не будет пароля пользователя'
+    assert user.is_active is True, 'Ожидалось, что поле пользователя "is_active" будет равно True'
 
 
 def test_invalid_user_registration(user_data: dict):
@@ -39,6 +41,6 @@ def test_invalid_user_registration(user_data: dict):
     response = api.post(url, invalid_user_data, format='json')
     response_content = response.json()
 
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert User.objects.count() == 0
-    assert response_content.get('email') is not None
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, 'Ожидался 400 статус-код ответа'
+    assert User.objects.count() == 0, 'Ожидалось, что количество пользователей в базе данных будет равно 0'
+    assert response_content.get('email') is not None, 'Ожидалось, что в теле ответа не будет поля "email"'
