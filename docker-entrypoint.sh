470202fd2552b9ab4d40d197dfa571d889df8625
s1veme <kpdpav@gmail.com>
2023-03-14 17:55:07 +0300

    fix(): fix path in docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+python src/manage.py makemigrations --no-input
+python src/manage.py migrate --no-input
+python src/manage.py collectstatic --no-input
 exec "$@"
