de0bb8e305112fc739a26d35d67bacd784343e4a
s1veme <kpdpav@gmail.com>
2023-03-14 18:42:26 +0300

    update(src/api): fix version api in urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('users/', include('api.v1.user.urls')),
+    path('v1/users/', include('api.v1.user.urls')),
     path('auth/', include('api.common.auth.urls')),
 ]
