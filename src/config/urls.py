6284fe5192eb2f8b3a24c1abcd6214c384343ee6
FrozZzy1 <frozzzy71@gmail.com>
2023-03-15 23:48:16 +0700

    update(src/config): add schema URLs

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
