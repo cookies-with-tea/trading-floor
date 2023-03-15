b2f11ea5c2966985760e3cee582912d3e8da85e5
Nermitor <dmatyukhin2006@mail.ru>
2023-03-15 13:35:55 +0700

    update(): restyling code after new style rules

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
@@ -12,4 +12,4 @@ export default defineConfig({
       bundler: 'vite',
     },
   },
-})
+});
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,27 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import IconTemplate from '@/components/common/IconTemplate.vue'
+import IconTemplate from '@/components/common/IconTemplate.vue';
 
-import 'virtual:svg-icons-register'
+import 'virtual:svg-icons-register';
 
 // TODO: После подключения шрифтов убрать комментарий.
 // import 'virtual:fonts.css'
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+import ElementPlus from 'element-plus';
 
-import ElementPlus from 'element-plus'
+import '@/styles/index.scss';
 
-import '@/styles/index.scss'
+const app = createApp(App);
 
-const app = createApp(App)
+app.component('IconTemplate', IconTemplate);
 
-app.component('IconTemplate', IconTemplate)
+app.use(createPinia());
 
-app.use(createPinia())
+app.use(router);
 
-app.use(router)
+app.use(ElementPlus);
 
-app.use(ElementPlus)
-
-app.mount('#app')
+app.mount('#app');
