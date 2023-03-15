85af76fa9fa8b3f8ea12bbeeb24919cf1265e042
Nermitor <dmatyukhin2006@mail.ru>
2023-03-15 13:01:17 +0700

    update(): add a proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
-import EslintPlugin from 'vite-plugin-eslint'
-import StyleLintPlugin from 'vite-plugin-stylelint'
-import VitePluginFonts from 'vite-plugin-fonts'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { fileURLToPath, URL } from 'node:url';
+import EslintPlugin from 'vite-plugin-eslint';
+import StyleLintPlugin from 'vite-plugin-stylelint';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import path from 'path'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import path from 'path';
 
 const styleLintConfig = StyleLintPlugin({
   files: ['src/**/*.{vue,scss}'],
   fix: true,
-})
+});
 
 const eslintConfig = EslintPlugin({
   fix: true,
   cache: false,
-})
+});
 
 // TODO: Убрать. Пример подключения шрифтов
 // const fontsConfig = VitePluginFonts({
@@ -37,21 +36,30 @@ const eslintConfig = EslintPlugin({
 
 const autoImportConfig = AutoImport({
   resolvers: [ElementPlusResolver()],
-})
+});
 
 const componentsConfig = Components({
   resolvers: [ElementPlusResolver()],
-})
+});
 
 const svgIconsConfig = createSvgIconsPlugin({
   iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
   symbolId: 'icon-[dir]-[name]',
   inject: 'body-first',
   customDomId: '__svg__icons__dom__',
-})
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
@@ -69,4 +77,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});
