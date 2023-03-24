0ee5cfb0c8d6caeb3d93adeb946ef30e3e1c1807
Nermitor <dmatyukhin2006@mail.ru>
2023-03-24 23:53:43 +0700

    update(src): refactor

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-// @ts-ignore
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 const styleLintConfig = StyleLintPlugin({
