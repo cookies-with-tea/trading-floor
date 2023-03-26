8d9525c3cc55a813d8b38bf8c5a7212a59c37e37
FrozZzy1 <frozzzy71@gmail.com>
2023-03-26 18:08:19 +0700

    update(src/api): add advertisement to the URLS

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('v1/users/', include('api.v1.user.urls')),
+    path('v1/advertisements/', include('api.v1.advertisement.urls')),
     path('auth/', include('api.common.auth.urls')),
 ]
