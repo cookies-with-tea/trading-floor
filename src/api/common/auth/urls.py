08c6549020535cd05f2a61524e681a77fc6ab6bd
s1veme <kpdpav@gmail.com>
2023-03-30 18:41:33 +0300

    fix(src/api/common/auth): delete slash in end urls

--- src/api/common/auth/urls.py
+++ src/api/common/auth/urls.py
@@ -4,5 +4,5 @@ from api.common.auth.views import AuthorizationGoogleAPIView, SingUpAPIView
 
 urlpatterns = [
     path('sign-up', SingUpAPIView.as_view(), name='sign-up'),
-    path('google/', AuthorizationGoogleAPIView.as_view(), name='authorization-google'),
+    path('google', AuthorizationGoogleAPIView.as_view(), name='authorization-google'),
 ]
