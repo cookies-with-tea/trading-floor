d61a17883ed451f5b4316d013d14fea5e3822b39
s1veme <kpdpav@gmail.com>
2023-03-30 18:30:09 +0300

    add(src/api/common/auth): add CredentialsModelSerializer, SignUpModelSerializer

--- src/api/common/auth/serializers.py
+++ src/api/common/auth/serializers.py
@@ -1,5 +1,36 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from apps.user.models import User
 
 
 class GoogleCredentialsSerializer(serializers.Serializer):
     authorization_code = serializers.CharField()
+
+
+class CredentialsModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['is_active']
+        read_only_fields = ['access', 'refresh']
+
+    def to_representation(self, instance: User):
+        data = super().to_representation(instance)
+
+        refresh: RefreshToken = RefreshToken.for_user(instance)
+
+        data.update(refresh=str(refresh))
+        data.update(access=str(refresh.access_token))
+
+        return data
+
+
+class SignUpModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'room_number',
+            'avatar',
+        ]
