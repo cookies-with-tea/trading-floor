d2e0f834a4d01e8aefe8ab7887cec1c7b39d9e86
FrozZzy1 <frozzzy71@gmail.com>
2023-03-30 21:06:59 +0700

    fix(src/api/v1/advertisement): delete unnecessary spaces

--- src/api/v1/advertisement/urls.py
+++ src/api/v1/advertisement/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from api.v1.advertisement.views import CreateAdvertisementViewSet
 
 router = routers.SimpleRouter()
-router.register('advertisements',  CreateAdvertisementViewSet)
+router.register('advertisements', CreateAdvertisementViewSet)
 
 urlpatterns = [] + router.urls
