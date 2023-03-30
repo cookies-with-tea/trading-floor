5fc63b98880d011b7208a58282dedbc4e3717333
FrozZzy1 <frozzzy71@gmail.com>
2023-03-31 02:33:57 +0700

    add(src/api/v1/advertisement): add `RetrieveAdvertisementAPIView` and `ListAdvertisementAPIView`, add it to the `urls`

--- src/api/v1/advertisement/urls.py
+++ src/api/v1/advertisement/urls.py
@@ -1,8 +1,16 @@
+from django.urls import path
 from rest_framework import routers
 
-from api.v1.advertisement.views import CreateAdvertisementViewSet
+from api.v1.advertisement.views import (
+    CreateAdvertisementViewSet,
+    ListAdvertisementAPIView,
+    RetrieveAdvertisementAPIView,
+)
 
 router = routers.SimpleRouter()
 router.register('advertisements', CreateAdvertisementViewSet)
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path('<int:pk>', RetrieveAdvertisementAPIView.as_view(), name='advertisement-detail'),
+    path('list', ListAdvertisementAPIView.as_view(), name='all-advertisement'),
+] + router.urls
