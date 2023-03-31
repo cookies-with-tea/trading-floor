99d9de6cfa7d907d84536ead1e31996f481a3cd1
s1veme <kpdpav@gmail.com>
2023-03-31 15:28:46 +0300

    delete(src/api/v1/advertisement): delete RetrieveAdvertisementAPIView, ListAdvertisementAPIView

--- src/api/v1/advertisement/views.py
+++ src/api/v1/advertisement/views.py
@@ -1,8 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from api.v1.advertisement.serializers import AdvertisementSerializer, ListAdvertisementSerializer
+from api.v1.advertisement.serializers import AdvertisementSerializer
 from apps.advertisement.models import Advertisement
 
 
@@ -15,15 +14,3 @@ class CreateAdvertisementViewSet(viewsets.ModelViewSet):
         request.data['author'] = request.user
 
         return super().create(request)
-
-
-class RetrieveAdvertisementAPIView(RetrieveAPIView):
-    queryset = Advertisement.objects.all()
-    serializer_class = AdvertisementSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class ListAdvertisementAPIView(ListAPIView):
-    queryset = Advertisement.objects.all()
-    serializer_class = ListAdvertisementSerializer
-    permission_classes = [IsAuthenticated]
