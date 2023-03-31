9fd4cbfaf590c85e81e254f85a831cfceb947576
s1veme <kpdpav@gmail.com>
2023-03-31 15:32:46 +0300

    update(src/api/v1/advertisements/tests/test_view): add additional checks on the content of the response body in test_create_advertisement

--- src/api/v1/advertisement/tests/test_view/test_advertisement.py
+++ src/api/v1/advertisement/tests/test_view/test_advertisement.py
@@ -19,6 +19,13 @@ def test_create_advertisement(api_client: APIClient, advertisement_data: dict, u
     assert Advertisement.objects.count() == 0, 'Ожидалось, что количество объявлений в базе данных будет равно 0'
 
     response = api_client.post(reverse('advertisements-list'), advertisement_data)
+    response_content = response.json()
 
     assert response.status_code == status.HTTP_201_CREATED, 'Ожидался 201 статус-код ответа'
     assert Advertisement.objects.count() == 1, 'Ожидалось, что количество объявлений в базе данных будет равно 1'
+    assert (
+        response_content['title'] == advertisement_data['title']
+    ), 'Поле "title" в теле ответа не соответствует изначальным данным'
+    assert (
+        response_content['description'] == advertisement_data['description']
+    ), 'Поле "description" в теле ответа не соответствует изначальным данным'
