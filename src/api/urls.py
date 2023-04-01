594db84e153daedbbd1cc2ad75e031bdff1c4dc7
s1veme <kpdpav@gmail.com>
2023-04-01 20:33:31 +0300

    update(src/api): update urls, delete namespace, add SpectacularAPIView

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('v1/', include(('api.v1.urls', 'v1'), namespace='v1')),
+    path('v1/', include(('api.v1.urls', 'v1'))),
     path('auth/', include('api.common.auth.urls')),
 ]
