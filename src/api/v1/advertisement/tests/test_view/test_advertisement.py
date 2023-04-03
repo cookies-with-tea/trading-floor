df97378993d62be72a36f87ad5071784e0ba0535
s1veme <kpdpav@gmail.com>
2023-04-03 21:18:45 +0300

    add(src/api/v1/advertisement/tests/test_view): add test for retrieve and list advertisement

--- src/api/v1/advertisement/tests/test_view/test_advertisement.py
+++ src/api/v1/advertisement/tests/test_view/test_advertisement.py
@@ -29,3 +29,44 @@ def test_create_advertisement(api_client: APIClient, advertisement_data: dict, u
     assert (
         response_content['description'] == advertisement_data['description']
     ), 'Поле "description" в теле ответа не соответствует изначальным данным'
+
+
+def test_retrieve_advertisement(api_client: APIClient, advertisement_factory) -> None:
+    advertisement: Advertisement = advertisement_factory()
+
+    api_client.force_authenticate(advertisement.author)
+
+    response = api_client.get(reverse('advertisements-detail', args=[advertisement.id]))
+    response_content = response.json()
+
+    assert response.status_code == status.HTTP_200_OK, 'Ожидался 200 статус-код ответа'
+    assert response_content['title'] == advertisement.title
+    assert response_content['description'] == advertisement.description
+
+
+def test_retrieve_invalid_id_advertisement(api_client, advertisement_factory) -> None:
+    advertisement: Advertisement = advertisement_factory()
+
+    api_client.force_authenticate(advertisement.author)
+
+    response = api_client.get(reverse('advertisements-detail', args=[2]))
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND, 'Ожидался 404 статус-код ответа'
+
+
+def test_list_advertisement(api_client, advertisement_factory) -> None:
+    advertisement: Advertisement = advertisement_factory()
+    for _ in range(19):
+        advertisement_factory()
+
+    assert Advertisement.objects.count() == 20, 'Ожидалось, что в базе данных будет 20 объявлений'
+
+    api_client.force_authenticate(advertisement.author)
+
+    response = api_client.get(reverse('advertisements-list'))
+    response_content = response.json()
+
+    assert response.status_code == status.HTTP_200_OK, 'Ожидался 200 статус-код ответа'
+    assert (
+        len(response_content) == 20
+    ), 'Ожидалось, что количество объектов в ответе будет равно количеству объектов в базе данных'
