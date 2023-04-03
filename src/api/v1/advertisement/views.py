65e8b66f99ec3957a716c43bf421ce46d944f627
FrozZzy1 <frozzzy71@gmail.com>
2023-04-03 18:24:21 +0700

    update(src/api/v1/advertisement): add dict `serializer_classes`, `default_serializer_class and method `get_serializer_class` to the `AdvertisementModelViewSet`

--- src/api/v1/advertisement/views.py
+++ src/api/v1/advertisement/views.py
@@ -1,11 +1,20 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from api.v1.advertisement.serializers import AdvertisementSerializer
+from api.v1.advertisement import serializers
 from apps.advertisement.models import Advertisement
 
 
 class AdvertisementModelViewSet(viewsets.ModelViewSet):
+    serializer_classes = {
+        'create': serializers.AdvertisementSerializer,
+        'retrieve': serializers.AdvertisementSerializer,
+        'list': serializers.AdvertisementListSerializer,
+    }
+
+    default_serializer_class = serializers.AdvertisementSerializer
     queryset = Advertisement.objects.all()
-    serializer_class = AdvertisementSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        return self.serializer_classes.get(self.action, self.default_serializer_class)
