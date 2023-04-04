42c96297b8132d365e32a65c0bc428488a0e16cb
s1veme <kpdpav@gmail.com>
2023-04-04 19:58:12 +0300

    add(src/api/v1/advertisement/tests/test_view): add test_advertisement_permission

---
+++ src/api/v1/advertisement/tests/test_view/test_advertisement_permission.py
@@ -0,0 +1,20 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from apps.user.models import User
+
+pytestmark = [
+    pytest.mark.django_db,
+]
+
+
+def test_get_with_not_rights(api_client: APIClient, user_factory) -> None:
+    user: User = user_factory()
+
+    api_client.force_authenticate(user)
+
+    response = api_client.get(reverse('advertisements-list'))
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
