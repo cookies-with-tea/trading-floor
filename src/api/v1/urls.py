2529a5e4e7bf7f0b720840c91beabf083fcfb6a0
s1veme <kpdpav@gmail.com>
2023-04-05 13:15:07 +0300

    add(src/api/v1): add router for advertisements

--- src/api/v1/urls.py
+++ src/api/v1/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
     path('users/', include('api.v1.user.urls')),
+    path('advertisements/', include('api.v1.advertisement.urls')),
 ]
