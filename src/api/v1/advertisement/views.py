ab6fc2f3e16cc3cbfe99e1886e374dc6825104d9
s1veme <kpdpav@gmail.com>
2023-04-05 13:16:29 +0300

    refactor(src/api/v1/advertisement): refactor the AdvertisementModelViewSet now uses SerializerClassMapMixin

--- src/api/v1/advertisement/views.py
+++ src/api/v1/advertisement/views.py
@@ -1,20 +1,18 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
 from api.v1.advertisement import serializers
 from apps.advertisement.models import Advertisement
+from apps.user.permissions import IsAuthenticatedAndIsActive
+from utils.mixins.views import SerializerClassMapMixin
 
 
-class AdvertisementModelViewSet(viewsets.ModelViewSet):
-    serializer_classes = {
+class AdvertisementModelViewSet(SerializerClassMapMixin, ModelViewSet):
+    serializer_class_map = {
         'create': serializers.AdvertisementSerializer,
         'retrieve': serializers.AdvertisementSerializer,
         'list': serializers.AdvertisementListSerializer,
     }
-
     default_serializer_class = serializers.AdvertisementSerializer
-    queryset = Advertisement.objects.all()
-    permission_classes = [IsAuthenticated]
 
-    def get_serializer_class(self):
-        return self.serializer_classes.get(self.action, self.default_serializer_class)
+    queryset = Advertisement.objects.all()
+    permission_classes = [IsAuthenticatedAndIsActive]
