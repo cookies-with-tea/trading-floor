4c506c8efda528611177d843d36c916c03bc159c
s1veme <kpdpav@gmail.com>
2023-04-07 14:58:51 +0300

    add(src/apps/user): add IsUser permission

--- src/apps/user/permissions.py
+++ src/apps/user/permissions.py
@@ -1,6 +1,12 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import BasePermission, IsAuthenticated
+from rest_framework.request import Request
 
 
 class IsAuthenticatedAndIsActive(IsAuthenticated):
     def has_permission(self, request, view):
         return super().has_permission(request, view) and request.user.is_active
+
+
+class IsUser(BasePermission):
+    def has_permission(self, request: Request, view):
+        return request.user
