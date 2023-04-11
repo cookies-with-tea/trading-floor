fe0b6be8fd2f9df66ef83d40d68c5bc97f6eb150
s1veme <kpdpav@gmail.com>
2023-04-11 22:29:52 +0300

    fix(src/api): delete unused imports

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('v1/', include(('api.v1.urls', 'v1'))),
