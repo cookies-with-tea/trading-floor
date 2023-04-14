5f737324ae7e5e431f0837e604a1439c310a7bbd
Nermitor <dmatyukhin2006@mail.ru>
2023-04-14 20:17:49 +0700

    delete(src/types): Удалены ненужные типы

--- src/types/authFormTypes.ts
+++ src/types/authFormTypes.ts
@@ -1,16 +1,3 @@
-export type LoginFormType = {
-  email: string;
-  password: string;
-};
-
-export type RegisterFormType = {
-  email: string;
-  firstName: string;
-  lastName?: string;
-  password: string;
-  roomNumber: number | null;
-};
-
 export type GoogleRegistrationFormType = {
   firstName: string;
   lastName: string;
