e47d51db98d70bbfc6e3c71b48444b2833e3d151
s1veme <kpdpav@gmail.com>
2023-04-17 19:18:41 +0300

    fix(src/api/v1/advertisement): delete field title in ImageSerializer, add image field

--- src/api/v1/advertisement/serializers.py
+++ src/api/v1/advertisement/serializers.py
@@ -8,7 +8,7 @@ class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
         fields = [
-            'title',
+            'image',
             'advertisement',
         ]
 
