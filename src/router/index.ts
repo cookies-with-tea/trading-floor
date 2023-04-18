75a960117ad833cd4aaee2d357baeb0bbb6335eb
Nermitor <dmatyukhin2006@mail.ru>
2023-04-19 00:56:30 +0700

    update(src/router): Защищены роуты

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import { ROUTE_NAMES } from '@/constants/routeNames';
+import { useTokens } from '@/composables/useTokens';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,13 +11,36 @@ const router = createRouter({
       component: DefaultLayout,
       children: [
         {
+          path: 'login',
+          name: ROUTE_NAMES.LoginPage,
+          component: async () => import('@/pages/LoginPage.vue'),
+          meta: {
+            isPublic: true,
+          },
+        },
+        {
           path: '/',
           name: ROUTE_NAMES.HomePage,
-          component: () => import('@/pages/HomePage.vue'),
+          component: async () => import('@/pages/HomePage.vue'),
         },
       ],
     },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const { getRefresh, getAccess } = useTokens(localStorage);
+
+  if (!to.matched.some((record) => record.meta?.isPublic)) {
+    const access = getAccess();
+    const refresh = getRefresh();
+
+    if (!access && !refresh) {
+      return next({ name: ROUTE_NAMES.LoginPage });
+    }
+  }
+
+  next();
 });
 
 export default router;
