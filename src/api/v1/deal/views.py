4f2a6080204f597270d3fb789bfaf34d81c40d92
FrozZzy1 <frozzzy71@gmail.com>
2023-05-05 13:18:43 +0700

    update(src/api/v1/deal): update inheriting and add filters at the queryset

--- src/api/v1/deal/views.py
+++ src/api/v1/deal/views.py
@@ -3,26 +3,27 @@ from rest_framework.viewsets import ModelViewSet
 from api.v1.deal import serializers
 from apps.deal.models import Deal
 from apps.user.permissions import IsAuthenticatedAndIsActive
+from utils.mixins.views import SerializerClassMapMixin
 
 
-class DealModelViewSet(ModelViewSet):
+class DealModelViewSet(SerializerClassMapMixin, ModelViewSet):
     serializer_class_map = {
         'retrieve': serializers.DealSerailizer,
         'create': serializers.CreateDealSerializer,
     }
     default_serializer_class = serializers.DealSerailizer
 
-    queryset = Deal.objects.all()
+    queryset = Deal.objects.filter(is_response=False)
     permission_classes = [IsAuthenticatedAndIsActive]
 
 
-class FeedBackModelViewSet(ModelViewSet):
+class FeedBackModelViewSet(SerializerClassMapMixin, ModelViewSet):
     serializer_class_map = {
         'retrieve': serializers.FeedbackSerializer,
-        'list': serializers.FeedbakSerializer,
+        'list': serializers.FeedbackSerializer,
         'create': serializers.CreateFeedbackSerializer,
     }
     default_serializer_class = serializers.FeedbackSerializer
 
-    queryset = Deal.objects.all()
+    queryset = Deal.objects.filter(is_response=True)
     permission_classes = [IsAuthenticatedAndIsActive]
