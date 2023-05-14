486cd7585bb0b48b3da247db1da4b9c415c6ad57
s1veme <kpdpav@gmail.com>
2023-05-14 20:57:31 +0300

    add(src/api/v1/user): add UpdateUserSerializer and add field avatar_color in UserDetailSerializer, UserProfileSerializer

--- src/api/v1/user/serializers.py
+++ src/api/v1/user/serializers.py
@@ -1,11 +1,11 @@
+from django.core.files.storage import default_storage
 from rest_framework import serializers
 
 from apps.user.models import User
+from utils.colors import generate_random_color
 
 
 class UserDetailSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)
-
     class Meta:
         model = User
         fields = [
@@ -15,28 +15,56 @@ class UserDetailSerializer(serializers.ModelSerializer):
             'last_name',
             'room_number',
             'avatar',
+            'avatar_color',
             'created_at',
             'updated_at',
             'is_active',
             'is_staff',
-            'user',
         ]
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)
-
     class Meta:
         model = User
         fields = [
             'id',
             'first_name',
-            'last_name',
             'room_number',
             'avatar',
+            'avatar_color',
             'created_at',
             'updated_at',
             'is_active',
             'is_staff',
-            'user',
         ]
+
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['avatar', 'first_name', 'last_name']
+
+    def to_representation(self, instance):
+        return UserDetailSerializer(instance).data
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        avatar = validated_data.pop('avatar', None)
+        instance = super().update(instance, validated_data)
+
+        if avatar:
+            self._process_avatar(instance, avatar)
+        else:
+            self._generate_avatar_color(instance)
+
+        instance.save()
+
+        return instance
+
+    def _process_avatar(self, instance, avatar):
+        if instance.avatar:
+            default_storage.delete(instance.avatar.name)
+        instance.avatar.save(avatar.name, avatar)
+        instance.avatar_color = None
+
+    def _generate_avatar_color(self, instance):
+        instance.avatar_color = generate_random_color()
