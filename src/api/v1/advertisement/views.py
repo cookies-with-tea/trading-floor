260e6acdb39db08cc493a29ba1c31af66c081680
s1veme <kpdpav@gmail.com>
2023-06-10 03:12:43 +0300

    add(src/api/v1/advertisement): add pagination for advertisement list

--- src/api/v1/advertisement/views.py
+++ src/api/v1/advertisement/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 
 from api.v1.advertisement import serializers
@@ -6,6 +7,12 @@ from api.v1.advertisement.filters import AdvertisementFilter
 from api.v1.advertisement.serializers import AdvertisementCategorySerializer
 from apps.advertisement.models import Advertisement, AdvertisementCategory
 from utils.mixins.views import SerializerClassMapMixin
+
+
+class AdvertisementListPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 20
 
 
 class AdvertisementModelViewSet(SerializerClassMapMixin, ModelViewSet):
@@ -16,8 +23,9 @@ class AdvertisementModelViewSet(SerializerClassMapMixin, ModelViewSet):
     }
     default_serializer_class = serializers.AdvertisementSerializer
 
-    queryset = Advertisement.objects.all()
+    queryset = Advertisement.objects.all().order_by('created_at')
     filterset_class = AdvertisementFilter
+    pagination_class = AdvertisementListPagination
 
 
 class AdvertisementCategoryListAPIView(ListAPIView):
